bb35514c4083f559129caa5f48966271e45f8e88
Wendy Wong <wendy.wong@gmail.com>
2023-03-22 18:15:37 -0400

    added cost limit

--- scripts/run_plink.sh
+++ scripts/run_plink.sh
@@ -7,4 +7,6 @@ dx run app-swiss-army-knife \
    -iin=demo_data:Data/AMR.bed \
    -iin=demo_data:Data/AMR.bim \
    -iin=demo_data:Data/AMR.fam \
-   -icmd="plink2 --bfile AMR --hwe 1e-5 --make-bed --out AMR_hwe" --destination demo_analysis:/results -y
+   -icmd="plink2 --bfile AMR --hwe 1e-5 --make-bed --out AMR_hwe" \
+   --destination demo_analysis:/results -y \
+   --cost-limit 10
