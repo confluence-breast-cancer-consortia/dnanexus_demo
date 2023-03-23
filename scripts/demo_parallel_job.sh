d4326b7c2bded2198e9e7b355ea9e3565006b4f6
Wendy Wong <wendy.wong@gmail.com>
2023-03-23 17:44:25 -0400

    Create demo_parallel_job.sh

---
+++ scripts/demo_parallel_job.sh
@@ -0,0 +1,6 @@
+for chrom in 1 2 3; do \
+dx run app-swiss-army-knife \
+   -iimage="shukwong/rstudio_with_gwas_tools:fd324eb9d3d2117fc37a157b66fa371a53693442" \
+   -iin=test.R 
+   -icmd="Rscript test.R ${chrom}" -y; \
+Done
